790b0789e37707721146c4211567c6b85649f4db
joaovitorferrei <84345484viktor@gmail.com>
2023-05-25 17:38:17 -0300

    movivimentação mause -> delta time

--- space/main.py
+++ space/main.py
@@ -1,6 +1,7 @@
 import pygame
 import os,sys,time
 
+
 '''
 Criando os Retangulos do Jogo
 
@@ -24,7 +25,7 @@ bgR1 = bg1.get_rect(center=((width/2,(height/2))))
 
 
 font = pygame.font.Font(os.path.join("assets","Font","Sigmar","Sigmar-Regular.ttf"),16)
-texto = font.render('S T A R - GAME', True,(65,105,225))
+texto = font.render('S P A C E - GAME', True,(65,105,225))
 recText = texto.get_rect(center=(100,10))
 loop = True
 relogio = pygame.time.Clock()
@@ -33,21 +34,24 @@ while loop:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             loop = False
+        #if event.type == pygame.MOUSEMOTION:
+        #   print(event.pos)
+        #    navRec.center = event.pos
 
-       
     #Limitando os frames  (FPS)
-    relogio.tick(30)
+    relogio.tick(120)
+    # print(pygame.mouse.get_pressed())
 
     # Atualizando os Quadros
-
+    navRec.center = pygame.mouse.get_pos()
     display.fill('black')
     display.blit(bg1, bgR1)    
 
     #utilizando o retangulo para poscionar a nave
+    
     display.blit(nave, navRec)
-    if navRec.y >=10:
-        navRec.y-=0.05 # type: ignore
-
+    #if navRec.y >=10:
+    #    navRec.y-=1 # type: ignore
     #display.blit(texto, (10,10))
     display.blit(texto, recText)
     
